7df43941b3bcdc2ddad85d0524d84fc65cd045ae
martjag00 <mamba@tlu.ee>
2019-11-21 19:43:16 +0200

    5. lesson end Mongo

--- server/server.js
+++ server/server.js
@@ -3,6 +3,38 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const DB = require("./database.js");
+const mongoose= require("mongoose");
+require('dotenv').config();
+
+var kittySchema = new mongoose.Schema({
+    name: String
+});
+
+var Kitten = mongoose.model('Kitten', kittySchema);
+
+const kitten1 = new Kitten({
+    name: "red cat 2"
+});
+
+
+const DB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0-5x1uj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+mongoose.connect(DB_URL)
+    .then (() => {
+        console.log("database success");
+        kitten1.save(err => {
+           if(err){
+               console.error("we had an error");
+           }
+           else{
+               console.log("success save");
+           }
+        });
+    })
+    .catch(err =>{
+        console.error("error happened",err);
+    });
+
 
 /* get all items*/
 app.get("/api/items", (req, res)=>{
