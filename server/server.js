378c996d46d7d397874a67a6f0a7056f9cef6d51
martjag00 <mamba@tlu.ee>
2019-11-21 20:52:38 +0200

    5 praksi lõpp

--- server/server.js
+++ server/server.js
@@ -2,49 +2,13 @@ const express = require('express');
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const DB = require("./database.js");
 const mongoose= require("mongoose");
 require('dotenv').config();
-
-var kittySchema = new mongoose.Schema({
-    name: String
-});
-
-var Kitten = mongoose.model('Kitten', kittySchema);
-
-const kitten1 = new Kitten({
-    name: "red cat 2"
-});
-
+const userRouter = require("./user.js");
 
 const DB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0-5x1uj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-mongoose.connect(DB_URL)
-    .then (() => {
-        console.log("database success");
-        kitten1.save(err => {
-           if(err){
-               console.error("we had an error");
-           }
-           else{
-               console.log("success save");
-           }
-        });
-    })
-    .catch(err =>{
-        console.error("error happened",err);
-    });
-
-
-/* get all items*/
-app.get("/api/items", (req, res)=>{
-    res.json(DB.getItems())
-});
-
-/*get item ID järgi*/
-app.get("/api/items/:itemId", (req, res)=>{
-    res.send(DB.getItem(req.params.itemId));
-});
+app.use(userRouter);
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
@@ -56,8 +20,19 @@ app.get('/items/*', (req, res) => {
 
 app.use(express.static('dist'));
 
-// heroku vajab process.env.PORT
-app.listen(PORT, () => {
-    console.log("Server started", PORT);
-    console.log(`http://localhost:${PORT}`);
-});
+function listen(){
+    // heroku vajab process.env.PORT
+    app.listen(PORT, () => {
+        console.log("Server started", PORT);
+        console.log(`http://localhost:${PORT}`);
+    });
+}
+
+mongoose.connect(DB_URL)
+    .then (() => {
+        console.log("database success");
+        listen();
+    })
+    .catch(err =>{
+        console.error("error happened",err);
+    });
